967c4fd384541708b97534554c18c62e7b2d2845
Jan Mrázek <email@honzamrazek.cz>
2026-05-25 21:45:45 +0200

    Normalize output high current values

--- jlcparts/datatables.py
+++ jlcparts/datatables.py
@@ -133,7 +133,7 @@ def normalizeAttribute(key, value):
                     "Non-Repetitive Peak Forward Surge Current", "Quiescent Supply Current",
                     "Input Offset Current(IOS)", "Receive Current", "Current - Collector(Ic)",
                     "Supply Current (Iq)", "Current - Input Bias(Ib)", "Current - Output Low(Iol)",
-                    "Current - Surge(Itsm@F)"]):
+                    "Current - Output High(Ioh)", "Current - Surge(Itsm@F)"]):
             currentListKeys = [
                 "non-repetitive peak forward surge current",
                 "quiescent supply current",
@@ -143,6 +143,7 @@ def normalizeAttribute(key, value):
                 "supply current (iq)",
                 "current - input bias(ib)",
                 "current - output low(iol)",
+                "current - output high(ioh)",
                 "current - surge(itsm@f)",
             ]
             if key in currentListKeys and isinstance(value, str) and "," in value:
