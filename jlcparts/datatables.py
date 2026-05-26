21ac5f700383b97a45b3af1b1c2a82e6c456927b
Jan Mrázek <email@honzamrazek.cz>
2026-05-26 02:23:18 +0200

    Normalize low level delay time values

--- jlcparts/datatables.py
+++ jlcparts/datatables.py
@@ -448,7 +448,7 @@ def normalizeAttribute(key, value):
                 "Propagation Delay Tp Hl", "Propagation Delay Tp Lh", "Max Propagation Delay",
                 "Maximum Propagation Delay", "Td(on)", "Block Erase Time(T Be)",
                 "Temperature Conversion Time", "Setup Time", "Acquisition Time",
-                "Hold Settling Time", "High Level Delay Time"]):
+                "Hold Settling Time", "High Level Delay Time", "Low Level Delay Time"]):
             if compoundValue(value) and "@" not in value:
                 if key in larr(["Propagation Delay (TPD)", "Propagation Delay Time", "Reset Timeout", "Settling Time", "Response Time (Tr)", "Time to Trip (Max)", "Td(Off)",
                         "Propagation Delay Tp Hl", "Propagation Delay Tp Lh", "Td(on)", "Block Erase Time(T Be)",
